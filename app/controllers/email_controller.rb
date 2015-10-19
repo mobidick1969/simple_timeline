529467847c738edfc68d706e127ec3dd4ddeb5a5
mobidick1969 <ec2-user@ip-172-31-20-84.us-west-2.compute.internal>
2015-10-19 15:49:00 +0000

    mail보내기 추가 성공

--- app/controllers/email_controller.rb
+++ app/controllers/email_controller.rb
@@ -2,7 +2,19 @@ class EmailController < ApplicationController
   def email_form
   end
   def email_send
-    @email = params[:email]
-    @content = params[:content]
+    email = params[:email]
+    title = params[:title]
+    content = params[:content]
+
+    gmail = Gmail.connect("mobidick","j!")
+    gmail.deliver do
+      to email
+      subject title
+      text_part do
+        body content
+      end
+    end
+
+    gmail.logout
   end
 end
