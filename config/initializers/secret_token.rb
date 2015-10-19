4f55e1696950c29e2c2d82917aa74d0d1cc36ba8
mobidick1969 <ec2-user@ip-172-31-20-84.us-west-2.compute.internal>
2015-10-19 15:22:42 +0000

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SimpleTimeline::Application.config.secret_key_base = '0225ff760114ce76d1dcb13662366336d64e24f8cdb5b625259b6e114129c26efa17f8218bb7c4db3c59ec04b700bd5206f5e0110e9459c4588680657c66bb57'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+SimpleTimeline::Application.config.session_store :cookie_store, key: '_simple_timeline_session'
